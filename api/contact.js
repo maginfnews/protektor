516fb4593b199d71747806997d808887ca06b0ff
maginfnews <maicon@magpass.com.br>
2025-10-25 13:10:41 -0300

    autoconfig

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,4 @@
-const https = require('https');
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
